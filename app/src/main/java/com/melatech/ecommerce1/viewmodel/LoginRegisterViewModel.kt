b5452123fbfcf68781f606b5cabc3a40fee0802f
melatech <maincontractor@gmail.com>
2023-05-04 17:52:07 +0100

    created createAccountWithEmailAndPassword function in LoginRegisterViewModel

--- app/src/main/java/com/melatech/ecommerce1/viewmodel/LoginRegisterViewModel.kt
+++ app/src/main/java/com/melatech/ecommerce1/viewmodel/LoginRegisterViewModel.kt
@@ -1,4 +1,33 @@
 package com.melatech.ecommerce1.viewmodel
 
-class LoginRegisterViewModel {
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.melatech.ecommerce1.data.User
+import com.melatech.ecommerce1.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginRegisterViewModel @Inject constructor(
+    private val fireBaseAuth: FirebaseAuth
+) : ViewModel() {
+
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    val register: Flow<Resource<FirebaseUser>> = _register
+
+    fun createAccountWithEmailAndPassword(user: User, password: String) {
+        fireBaseAuth.createUserWithEmailAndPassword(user.email, password)
+            .addOnSuccessListener {
+                it.user?.let {
+                    _register.value = Resource.Success(it)
+                }
+            }
+            .addOnFailureListener {
+                _register.value = Resource.Error(it.message.toString())
+
+            }
+    }
 }
